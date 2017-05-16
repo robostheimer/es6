e12eb010cd60c5995f5f5a720267fa103f90512b
Rob Ostheimer <robostheimer@yahoo.com>
2017-05-15 22:42:08 -0700

    memoize fetches; good start

--- src/helpers/memoize.js
+++ src/helpers/memoize.js
@@ -1,9 +1,33 @@
 'use strict'
+const cache = {};
 
-export function memoize(key) {
-  this.values = this.values || {};
-
-  return this.values[key] !== undefined ?
-    this.values[key]:
-    this.values[key] = this.apply(this, arguments);
+export function memoized(key) {
+  let data = data || {};
+  return data[key] !== undefined ? data[key] : data[key] = this.apply(this, arguments);
 }
+
+export function memoize() {
+  let args = arguments;
+  let name = args[0].name;
+  let fn = args[0].fn;
+
+  if(!cache[name]) {
+    cache[name] = fn(). then((data) => {
+      return data.json();
+    })
+  }
+
+  return cache[name];
+
+}
+
+
+
+// var memoize = function(f) {  
+//    var cache = {};   
+//     return function() {     
+//       var arg_str = JSON.stringify(arguments);     
+//       cache[arg_str] = cache[arg_str] || f.apply(f, arguments);     r
+//       eturn cache[arg_str];  
+//     };
+//   };
